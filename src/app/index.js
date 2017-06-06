86cecdc08911c874eb2ce9c64dd54c9cd733c5c8
Andrii Kit <andrii.labyk@gmail.com>
2017-06-06 22:56:38 +0300

    8 lesson

--- src/app/index.js
+++ src/app/index.js
@@ -1,22 +1,67 @@
-import React from 'react';
-import { render } from 'react-dom';
-import {Router, Route, browserHistory} from 'react-router';
+// import React from 'react';
+// import { render } from 'react-dom';
+//
+// import { Main } from './components/Main';
+// import { User } from './components/User';
+//
+// class App extends React.Component {
+//
+//     constructor() {
+//         super();
+//         this.state = {
+//             username: 'Ivan',
+//         };
+//     }
+//
+//     changeUsername(newName) {
+//         this.setState({
+//             username:newName,
+//         });
+//     }
+//
+//     render() {
+//
+//         return (
+//             <div className="container">
+//                 <Main changeUsername={this.changeUsername.bind(this)} />
+//                 <User username={this.state.username} />
+//             </div>
+//         );
+//     }
+// }
+//
+// render(<App/>, document.getElementById('app'));
 
-import { Root } from './components/Root';
-import { Home } from './components/Home';
-import { User } from './components/User';
+import { createStore } from 'redux';
 
-class App extends React.Component {
-
-    render() {
-
-        return (
-            <Router history={browserHistory}>
-                <Route path={'user'} component={User} />
-                <Route path={'home'} component={Home} />
-            </Router>
-        );
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'ADD':
+            state = state + action.payload;
+            break;
+        case 'SUBTRACT':
+            state = state - action.payload;
     }
-}
+    return state;
+};
 
-render(<App/>, document.getElementById('app'));
+const store = createStore(reducer, 1);
+
+store.subscribe(() => {
+    console.log('store updated', store.getState());
+});
+
+store.dispatch({
+    type: 'ADD',
+    payload: 10,
+});
+
+store.dispatch({
+    type: 'ADD',
+    payload: 100,
+});
+
+store.dispatch({
+    type: 'SUBTRACT',
+    payload: 200,
+});
